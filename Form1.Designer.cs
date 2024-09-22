4192142ef628bef2809c2083cf5a59f266ceb106
cqiraan <cqiraan01@gmail.com>
2024-09-22 16:40:04 +0200

    added a button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(362, 169);
+            this.button1.Location = new System.Drawing.Point(212, 210);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
@@ -44,7 +44,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(363, 214);
+            this.button2.Location = new System.Drawing.Point(293, 210);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
@@ -53,7 +53,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(411, 283);
+            this.button3.Location = new System.Drawing.Point(304, 265);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 6;
